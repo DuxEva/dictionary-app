d52d0769ee351d9f48957889030a50414712a3a1
DuvEva <evariste.dusingizimana@amalitechtraining.org>
2024-07-01 23:49:34 +0200

    make the app responsive

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -32,7 +32,7 @@ const ContentContainer = ({ meaning }: { meaning: Meaning }) => {
     <div>
       <Margindevider value={meaning.partOfSpeech} />
       <h3 className="text-lg mt-5 text-light-4 mb-5">Meaning</h3>
-      <ul className="pl-[4rem] list-disc marker:text-purple-1">
+      <ul className="pl-[4rem] max-sm:pl-[2rem] list-disc marker:text-purple-1">
         {meaning.definitions.map((definition, index) => (
           <li key={index} className="mb-5">
             {definition.definition}
